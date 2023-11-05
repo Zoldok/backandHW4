ae7bbf75a94bd58224d83809201a14e5275b77cc
Zoldok <max_kyrtimov@mail.ru>
2023-11-05 21:22:04 +0300

    fix/ users

--- src/app.js
+++ src/app.js
@@ -1,20 +1,35 @@
 const express = require('express');
 const dotenv = require('dotenv')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const mongoose = require('mongoose')
 const userRouter = require('./routes/users')
+const loggerOne = require('./middlewares/loggerOne')
+
 
 dotenv.config()
-const app = express();
 
 const {
     PORT = 3000, 
-    API_URL = "http://127.0.0.1"
+    API_URL = "http://127.0.0.1",
+    MONGO_URL = "mongodb://127.0.0.1:27017/test"
 } = process.env;
+
+
+mongoose
+  .connect(MONGO_URL)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("Failed to connect to MongoDB", err));
+const app = express();
 
 const helloWorld = (request, response) => {
     response.status(200),
     response.send('Hello, World!')
 }
 
+app.use(cors())
+app.use(loggerOne)
+app.use(bodyParser.json())
 app.get('/', helloWorld)
 
 app.post('/', (request, response) => {
--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,24 +1,51 @@
-const { request, response } = require("express");
+// const { request, response } = require("express");
+const User = require('../models/user')
 
 const getUsers = (request, response) => {
-  const { user_id } = request.params
-  response.status(200), response.send(`User with id: ${user_id}`)
+return User.find({}).then((data) => {
+  response.status(200).send(data)
+})
 }
 
 const getUser = (request, response) => {
-  //Get user
+    const { user_id } = request.params
+    return User.findById(user_id).then(
+      (user) => {
+        response.status(200), response.send(user)
+      }
+    ).catch(e => response.status(500).send(e.message))
+    
 }
 
 const createUser = (request, response) => {
   //Create new user
+  // response.status(201), 
+  // response.send(request.body)
+  return User.create({...request.body}).then(
+    (user) => {
+      response.status(201).send(user)
+    }
+  ).catch(e => response.status(500).send(e.message))
 }
 
 const updateUser = (request, response) => {
   //Update user
+  const { user_id } = request.params
+  return User.findByIdAndUpdate(user_id, {...request.body}).then(
+    (user) => {
+      response.status(200), response.send(user)
+    }
+  ).catch(e => response.status(500).send(e.message))
 }
 
 const deleteUser = (request, response) => {
   //Dekete new user
+  const { user_id } = request.params
+  return User.findByIdAndDelete(user_id).then(
+    (user) => {
+      response.status(200), response.send("Success")
+    }
+  ).catch(e => response.status(500).send(e.message))
 }
 
 module.exports = {
